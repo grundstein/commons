a55500254dcdd74ec7126566aada20b2d4572041
jascha <jascha@jaeh.at>
2020-04-10 10:17:07 +0200

    middleware/listener: get host and port via args

--- src/middleware/listener.mjs
+++ src/middleware/listener.mjs
@@ -1,6 +1,6 @@
 import { log } from '../log.mjs'
 
-export const listener = startTime => () => {
+export const listener = ({ startTime, host, port }) => () => {
   log.success('Mainthread started', `pid: ${process.pid}`)
   log(`server listening to ${host}:${port}`)
   log.timeTaken(startTime, 'startup needed:')
