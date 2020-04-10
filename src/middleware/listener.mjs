50ba2da44083150ab0dd74514c770c3e9287d2a9
jascha <jascha@jaeh.at>
2020-04-10 11:11:47 +0200

    middleware/listener: format

--- src/middleware/listener.mjs
+++ src/middleware/listener.mjs
@@ -3,5 +3,6 @@ import { log } from '../log.mjs'
 export const listener = ({ startTime, host, port }) => () => {
   log.success('Mainthread started', `pid: ${process.pid}`)
   log(`server listening to ${host}:${port}`)
+
   log.timeTaken(startTime, 'startup needed:')
 }
