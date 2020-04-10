682a4ccd0d5a356e7f78404c5af7c77a6f5a85fd
jascha <jascha@jaeh.at>
2020-04-10 12:04:43 +0200

    lib.respond: use more data from payload

--- src/lib/respond.mjs
+++ src/lib/respond.mjs
@@ -1,10 +1,14 @@
 import { log } from '../log.mjs'
 import { formatLog } from './formatLog.mjs'
 
-export const respond = (res, payload = {}) => {
-  const startTime = log.hrtime()
-
-  const { code = 500, body = '500 - Server Error', headers } = payload
+export const respond = (req, res, payload = {}) => {
+  const {
+    body = '500 - Server Error',
+    code = 500,
+    headers = [],
+    time = log.hrtime(),
+    type = 'response',
+  } = payload
 
   const head = {
     'Content-Type': 'text/plain; charset=utf-8',
@@ -16,5 +20,5 @@ export const respond = (res, payload = {}) => {
   res.writeHead(code, head)
   res.end(body)
 
-  formatLog(req, res, { time: startTime, type: 'response' })
+  formatLog(req, res, { time, type })
 }
