5500945a68d6ca871a952ce78c06153e332a4258
jascha <jascha@jaeh.at>
2020-04-21 12:33:31 +0200

    lib/getHostname: correctly return a string stripped of port

--- src/lib/getHostname.mjs
+++ src/lib/getHostname.mjs
@@ -2,7 +2,7 @@ export const getHostname = r => {
   const hostname = r.hostname || r.headers['x-forwarded-host'] || r.headers.host || ''
 
   if (hostname.includes(':')) {
-    return hostname.split(':')
+    return hostname.split(':')[0]
   }
 
   return hostname
