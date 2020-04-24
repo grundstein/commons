80c7e60255f8d5e7d9cb4e3019d0d0187cda58c0
jascha <jascha@jaeh.at>
2020-04-24 11:49:18 +0200

    export fs and cli libs

--- src/index.mjs
+++ src/index.mjs
@@ -1,6 +1,8 @@
 export { log } from './log.mjs'
 
 export { default as is } from '@magic/types'
+export { default as fs } from '@magic/fs'
+export { default as cli } from '@magic/cli'
 
 export * as lib from './lib/index.mjs'
 export * as middleware from './middleware/index.mjs'
