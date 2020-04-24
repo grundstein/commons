290211c021b1e7633d6ae479acc4593eea9c664c
jascha <jascha@jaeh.at>
2020-04-24 12:43:41 +0200

    export @magic/error

--- src/index.mjs
+++ src/index.mjs
@@ -3,6 +3,7 @@ export { log } from './log.mjs'
 export { default as is } from '@magic/types'
 export { default as fs } from '@magic/fs'
 export { default as cli } from '@magic/cli'
+export { default as error } from '@magic/error'
 
 export * as lib from './lib/index.mjs'
 export * as middleware from './middleware/index.mjs'
