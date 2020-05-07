898d2d8a009962484cd624359193ae8ab1af7150
jascha <jascha@jaeh.at>
2020-05-07 17:46:45 +0200

    lib/formatLog: use getRequestDuration and getCurrentDate

--- src/lib/formatLog.mjs
+++ src/lib/formatLog.mjs
@@ -1,59 +1,14 @@
-import { log } from '../log.mjs'
+import log from '../log.mjs'
+import { getRequestDuration } from './getRequestDuration.mjs'
+import { getCurrentDate } from './getCurrentDate.mjs'
 
 export const formatLog = (req, res, { time, type = 'request' }) => {
   const { statusCode } = res
   const { url } = req
 
-  const [s, ns] = process.hrtime(time)
-  let span = s * 1000000 + ns / 1000
-  let unit = 'ns'
+  const duration = getRequestDuration(time)
 
-  if (span > 1500000) {
-    unit = 's'
-    span = span / 1000000
-  } else if (ns > 1500) {
-    unit = 'ms'
-    span = span / 1000
-  }
-
-  span = span.toFixed(1)
-
-  const duration = `${span}${unit}`
-
-  const date = new Date()
-  let day = date.getDate()
-  let month = date.getMonth() + 1
-  const year = date.getFullYear()
-  let hour = date.getHours()
-  let minute = date.getMinutes()
-  let second = date.getSeconds()
-  let millisecond = date.getMilliseconds()
-
-  if (day < 10) {
-    day = `0${day}`
-  }
-
-  if (month < 10) {
-    month = `0${month}`
-  }
-  if (hour < 10) {
-    hour = `0${hour}`
-  }
-  if (minute < 10) {
-    minute = `0${minute}`
-  }
-  if (second < 10) {
-    second = `0${second}`
-  }
-  if (millisecond < 100) {
-    if (millisecond < 10) {
-      millisecond = `0${millisecond}`
-    }
-    millisecond = `0${millisecond}`
-  }
-
-  const dayString = `${year}/${month}/${day}`
-  const timeString = `${hour}:${minute}:${second}:${millisecond}`
+  const timeData = getCurrentDate()
 
   const response = [
     '{',
@@ -61,10 +16,10 @@ export const formatLog = (req, res, { time, type = 'request' }) => {
     statusCode,
     '", ',
     '"day": "',
-    dayString,
+    timeData.date,
     '", ',
     '"time": "',
-    timeString,
+    timeData.time,
     '", ',
     '"duration": "',
     duration,
