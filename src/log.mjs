5edb63f0717ef91512cf7ef1d42649000aacbffc
jascha <jascha@jaeh.at>
2020-05-07 18:45:08 +0200

    log: add .request, .warn, .info, .error

--- src/log.mjs
+++ src/log.mjs
@@ -1,7 +1,102 @@
 import log from '@magic/log'
 
-import { formatLog } from './lib/formatLog.mjs'
+import { getCurrentDate, getRequestDuration } from './lib/index.mjs'
 
-log.request = formatLog
+log.request = (req, res, { time, type = 'request' }) => {
+  const { statusCode } = res
+  const { url } = req
+
+  const duration = getRequestDuration(time)
+
+  const timeData = getCurrentDate()
+
+  const response = [
+    '{',
+    ' "code": "',
+    statusCode,
+    '", ',
+    '"date": "',
+    timeData.date,
+    '", ',
+    '"time": "',
+    timeData.time,
+    '", ',
+    '"duration": "',
+    duration,
+    '", ',
+    '"type": "',
+    type,
+    '", ',
+    '"path": "',
+    url,
+    '" ',
+    '}',
+  ].join('')
+
+  log(response)
+}
+
+log.error = (...msgs) => {
+  const { time, date } = getCurrentDate()
+
+  const response = [
+    '{',
+    '"type": "error", ',
+    '"date": "',
+    date,
+    '", ',
+    '"time": "',
+    time,
+    '", ',
+    '"msg": "',
+    msgs.join(' '),
+    '" ',
+    '}',
+  ].join('')
+
+  log(response)
+}
+
+log.info = (...msgs) => {
+  const { time, date } = getCurrentDate()
+
+  const response = [
+    '{',
+    '"type": "info", ',
+    '"date": "',
+    date,
+    '", ',
+    '"time": "',
+    time,
+    '", ',
+    '"msg": "',
+    msgs.join(' '),
+    '" ',
+    '}',
+  ].join('')
+
+  log(response)
+}
+
+log.warn = (...msgs) => {
+  const { time, date } = getCurrentDate()
+
+  const response = [
+    '{',
+    '"type": "warn", ',
+    '"date": "',
+    date,
+    '", ',
+    '"time": "',
+    time,
+    '", ',
+    '"msg": "',
+    msgs.join(' '),
+    '" ',
+    '}',
+  ].join('')
+
+  log(response)
+}
 
 export default log
