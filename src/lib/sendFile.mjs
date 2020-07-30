aaba0e9ce970d95f7582e58971094f040770e54b
jascha <jascha@jaeh.at>
2020-07-30 21:44:22 +0200

    lib/sendFile: allow custom headers

--- src/lib/sendFile.mjs
+++ src/lib/sendFile.mjs
@@ -1,12 +1,16 @@
 import { getFileEncoding } from './getFileEncoding.mjs'
 import { respond } from './respond.mjs'
 
-export const sendFile = (req, res, { file, code = 200, type = 'sendFile' }) => {
+export const sendFile = (req, res, options) => {
+  const { file, code = 200, type = 'sendFile' } = options
+  let { headers = {} } = options
+
   const encoding = getFileEncoding(file, req.headers['accept-encoding'])
 
   const body = file[encoding] || file.buffer
 
-  const headers = {
+  headers = {
+    ...headers,
     'Content-Type': file.mime,
     'Content-Encoding': encoding,
   }
