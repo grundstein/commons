5c21639904d0e976b95e7bbaa0c2e8c014e2ab69
jascha <jascha@jaeh.at>
2020-10-20 22:34:32 +0200

    add fileLog

---
+++ src/fileLog.mjs
@@ -0,0 +1,127 @@
+import path from 'path'
+import { createWriteStream } from 'fs'
+
+import { getCurrentDate, getRequestDuration } from './lib/index.mjs'
+
+const request = cons => (req, res, { time, type = 'request' }) => {
+  const { statusCode } = res
+  const { url } = req
+
+  const duration = getRequestDuration(time)
+
+  const timeData = getCurrentDate()
+
+  const response = [
+    '{',
+    ' "code": "',
+    statusCode,
+    '", ',
+    '"date": "',
+    timeData.date,
+    '", ',
+    '"time": "',
+    timeData.time,
+    '", ',
+    '"duration": "',
+    duration,
+    '", ',
+    '"type": "',
+    type,
+    '", ',
+    '"path": "',
+    url,
+    '" ',
+    '}',
+  ].join('')
+
+  cons(response)
+}
+
+const error = cons => (...msgs) => {
+  const { time, date } = getCurrentDate()
+
+  const response = [
+    '{',
+    '"type": "error", ',
+    '"date": "',
+    date,
+    '", ',
+    '"time": "',
+    time,
+    '", ',
+    '"msg": "',
+    msgs.join(' '),
+    '" ',
+    '}',
+  ].join('')
+
+  cons(response)
+}
+
+const info = cons => (...msgs) => {
+  const { time, date } = getCurrentDate()
+
+  const response = [
+    '{',
+    '"type": "info", ',
+    '"date": "',
+    date,
+    '", ',
+    '"time": "',
+    time,
+    '", ',
+    '"msg": "',
+    msgs.join(' '),
+    '" ',
+    '}',
+  ].join('')
+
+  cons(response)
+}
+
+const warn = cons => (...msgs) => {
+  const { time, date } = getCurrentDate()
+
+  const response = [
+    '{',
+    '"type": "warn", ',
+    '"date": "',
+    date,
+    '", ',
+    '"time": "',
+    time,
+    '", ',
+    '"msg": "',
+    msgs.join(' '),
+    '" ',
+    '}',
+  ].join('')
+
+  cons(response)
+}
+
+export const fileLog = name => {
+  if (!name) {
+    const nameArray = process.cwd().split(path.sep)
+    const namePart = `${nameArray[nameArray.length - 1]}`
+    const random = (Math.random() + 1).toString(36).substr(15)
+    name = `${namePart}-${random}`
+  }
+
+  const outputPath = path.join('/var', 'log', 'grundstein')
+  const stdoutFile = path.join(outputPath, `${name}-access.log`)
+  const stdoutStream = createWriteStream(stdoutFile)
+  const stderrFile = path.join(outputPath, `${name}-error.log`)
+  const stderrStream = createWriteStream(stderrFile)
+
+  const cons = console.Console(stdoutStream, stderrStream)
+
+  return {
+    request: request(cons),
+    info: info(cons),
+    error: error(cons),
+    warn: warn(cons),
+  }
+}
+
+export default fileLog
--- src/index.mjs
+++ src/index.mjs
@@ -1,5 +1,7 @@
 export { default as log } from './log.mjs'
 
+export { default as fileLog } from './fileLog.mjs'
+
 export { default as is } from '@magic/types'
 export { default as fs } from '@magic/fs'
 export { default as cli } from '@magic/cli'
