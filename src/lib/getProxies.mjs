1a98dcae07d62b5116555708e433ac66080ecfde
jascha <jascha@jaeh.at>
2020-10-22 00:29:50 +0200

    src/lib/getProxies.mjs: readFile uses utf8

--- src/lib/getProxies.mjs
+++ src/lib/getProxies.mjs
@@ -2,7 +2,7 @@ import fs from '@magic/fs'
 
 export const getProxies = async config => {
   try {
-    const proxyFileContents = await fs.readFile(config.proxyFile)
+    const proxyFileContents = await fs.readFile(config.proxyFile, 'utf8')
 
     /* read proxies, delimited by newlines, filter out empty lines */
     const proxies = proxyFileContents.split('\n').filter(a => a)
