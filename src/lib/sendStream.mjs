fc32a354f582f2099a706bec5b35f1030b3bd37b
jascha <jascha@jaeh.at>
2020-10-22 03:30:30 +0200

    sendStream: only start stream once

--- src/lib/sendStream.mjs
+++ src/lib/sendStream.mjs
@@ -4,10 +4,21 @@ export const sendStream = (req, res, options) => {
   const { file, headers = {} } = options
   const { range } = req.headers
 
-  if (range) {
+  let statusCode = 200
+
+  let start = 0
+  let end = file.size - 1
+
+  if (!range) {
+    headers['Content-Length'] = file.size
+    headers['Content-Type'] = file.mime
+    headers['Accept-Ranges'] = 'bytes'
+  } else {
+    statusCode = 206
+
     const [startByte, endByte] = range.replace(/bytes=/, '').split('-')
-    const start = parseInt(startByte, 10)
-    const end = endByte ? parseInt(endByte, 10) : file.size - 1
+    start = parseInt(startByte, 10)
+    end = endByte ? parseInt(endByte, 10) : file.size - 1
 
     if (start >= file.size) {
       const errorString = `Requested range not satisfiable ${start} >= ${file.size}`
@@ -20,20 +31,25 @@ export const sendStream = (req, res, options) => {
       return
     }
 
+
     const chunksize = end - start + 1
 
     headers['Content-Range'] = `bytes ${start}-${end}/${file.size}`
     headers['Accept-Ranges'] = 'bytes'
     headers['Content-Length'] = chunksize
     headers['Content-Type'] = file.mime
-
-    res.writeHead(206, headers)
-    createReadStream(file.path, { start, end }).pipe(res)
-  } else {
-    headers['Content-Length'] = file.size
-    headers['Content-Type'] = file.mime
-
-    res.writeHead(200, headers)
-    createReadStream(file.path).pipe(res)
   }
+
+  res.writeHead(statusCode, headers)
+
+  const readStream = createReadStream(file.path, { start, end })
+  readStream.setEncoding('utf8')
+
+  readStream.on('open', () => {
+    readStream.pipe(res)
+  })
+
+  readStream.on('error', err => {
+    res.end(err)
+  })
 }
