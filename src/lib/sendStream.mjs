73271eb1cd58c08151272893635cf8c19d3e14f0
jascha <jascha@jaeh.at>
2020-10-22 04:42:11 +0200

    src/lib/sendStream.mjs: unify functionality

--- src/lib/sendStream.mjs
+++ src/lib/sendStream.mjs
@@ -4,58 +4,52 @@ export const sendStream = (req, res, options) => {
   const { file, headers = {} } = options
   const { range } = req.headers
 
-  let statusCode = 200
+  const statusCode = 206
 
   let start = 0
   let end = file.size - 1
 
-  if (file.mime === 'application/javascript' || file.mime === 'application/css') {
-    file.mime = file.mime.replace('application', 'text')
-  }
+  // if (file.mime === 'application/javascript' || file.mime === 'application/css') {
+  //   file.mime = file.mime.replace('application', 'text')
+  // }
 
-  if (!range) {
-    headers['Content-Length'] = file.size
-    headers['Content-Type'] = file.mime
-    headers['Accept-Ranges'] = 'bytes'
-  } else {
-    statusCode = 206
-
+  if (range) {
     const [startByte, endByte] = range.replace(/bytes=/, '').split('-')
     start = parseInt(startByte, 10)
     end = endByte ? parseInt(endByte, 10) : file.size - 1
-
-    if (start >= file.size) {
-      const errorString = `Requested range not satisfiable ${start} >= ${file.size}`
-      const errorBuffer = Buffer.from(errorString)
-
-      headers['Content-Type'] = 'text/plain'
-      headers['Content-Length'] = Buffer.byteLength(errorBuffer)
-
-      res.writeHead(416, headers)
-      res.end(errorMessage)
-      return
-    }
-
-    const chunksize = end - start + 1
-
-    headers['Content-Range'] = `bytes ${start}-${end}/${file.size}`
-    headers['Accept-Ranges'] = 'bytes'
-    headers['Content-Length'] = chunksize
-    headers['Content-Type'] = file.mime
   }
 
-  res.writeHead(statusCode, headers)
+  if (start >= file.size) {
+    const errorString = `Requested range not satisfiable ${start} >= ${file.size}`
+    const errorBuffer = Buffer.from(errorString)
 
-  const readStream = createReadStream(file.path, { start, end })
-  readStream.setEncoding('utf8')
+    headers['Content-Type'] = 'text/plain'
+    headers['Content-Length'] = Buffer.byteLength(errorBuffer)
 
-  readStream.on('open', () => {
-    console.log('open stream')
-    readStream.pipe(res)
-  })
+    res.writeHead(416, headers)
+    res.end(errorMessage)
+    return
+  }
 
-  readStream.on('error', err => {
-    console.log('end stream')
-    res.end(err)
-  })
+  const chunksize = end - start + 1
+
+  headers['Content-Range'] = `bytes ${start}-${end}/${file.size}`
+  headers['Accept-Ranges'] = 'bytes'
+  headers['Content-Length'] = chunksize
+  headers['Content-Type'] = file.mime
+}
+
+res.writeHead(statusCode, headers)
+
+const readStream = createReadStream(file.path, { start, end })
+
+readStream.on('open', () => {
+  console.log('open stream')
+  readStream.pipe(res)
+})
+
+readStream.on('error', err => {
+  console.log('end stream')
+  res.end(err)
+})
 }
