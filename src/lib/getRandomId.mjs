621770c91c9ee7d818982d559941c268dbbb47f2
jaeh <jascha@jaeh.at>
2021-05-01 00:45:49 +0200

    getRandomId: check if buffer is defined

--- src/lib/getRandomId.mjs
+++ src/lib/getRandomId.mjs
@@ -1,15 +1,15 @@
 import crypto from 'crypto'
 
-import log from '@magic/log'
+import log from '../log.mjs'
 
 export const getRandomId = () =>
-  new Promise((resolve, reject) =>
+  new Promise(resolve =>
     crypto.randomBytes(12, (err, buffer) => {
       // something went wrong with /dev/urandom.
       // lets generate a simple numeric id.
       // this has a higher risk of id clashes in our logs,
       // but at least the progress won't die on us.
-      if (err) {
+      if (err || !buffer) {
         const id = Math.floor(Math.Random() * 1000000)
         log.server.error(err)
         resolve(id)
