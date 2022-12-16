98c34d247b0c9070a6b1a8af394afdf0d3548202
jaeh <jascha@jaeh.at>
2022-12-16 19:45:54 +0100

    lib/getHostname: handle both http 1.1 and http 2 request structures

--- src/lib/getHostname.mjs
+++ src/lib/getHostname.mjs
@@ -1,5 +1,28 @@
+import http2 from 'node:http2'
+
+const { HTTP2_HEADER_AUTHORITY } = http2.constants
+
 export const getHostname = headers => {
-  const authority = headers['x-forwarded-for'] || headers[':authority'] || headers.host
+  if (!headers) {
+    return ''
+  }
+
+  // http 1.1
+  if (headers.headers) {
+    const { hostname } = headers
+    const head = headers.headers
+
+    const host = head.host || head['x-forwarded-for'] || hostname || ''
+
+    if (host.includes(':')) {
+      return host.split(':')[0]
+    }
+
+    return host
+  }
+
+  // http 2
+  const authority = headers['x-forwarded-for'] || headers[HTTP2_HEADER_AUTHORITY] || headers.host
 
   if (authority.includes(':')) {
     return authority.split(':')[0]
