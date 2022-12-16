8dc31d847a078d4f88e40e1510f5384182b882fa
jaeh <jascha@jaeh.at>
2022-12-16 19:45:20 +0100

    lib/getClientIp: handle both 1.1 and 2 client ip scenarios

--- src/lib/getClientIp.mjs
+++ src/lib/getClientIp.mjs
@@ -5,6 +5,34 @@ import { cleanIpAddress } from './cleanIpAddress.mjs'
  * Returns the IP address from the headers, if it exists.
  */
 export const getClientIp = (stream = {}, headers = {}, full = false) => {
+  if (stream.connection && stream.socket) {
+    const req = stream
+    const { connection = {} } = req
+    if (is.ip(connection.remoteAddress)) {
+      return cleanIpAddress(connection.remoteAddress, full)
+    }
+
+    if (is.ip(connection?.socket?.remoteAddress)) {
+      return cleanIpAddress(connection.socket.remoteAddress, full)
+    }
+
+    const { socket = {} } = req
+    if (is.ip(socket.remoteAddress)) {
+      return cleanIpAddress(socket.remoteAddress, full)
+    }
+
+    const { info = {} } = req
+    if (is.ip(info.remoteAddress)) {
+      return cleanIpAddress(info.remoteAddress, full)
+    }
+
+    const { requestContext = {} } = req
+    const { identity = {} } = requestContext
+    if (is.ip(identity.sourceIp)) {
+      return cleanIpAddress(identity.sourceIp, full)
+    }
+  }
+
   const keys = [
     'x-forwarded-for',
     'x-forwarded',
