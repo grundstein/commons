13582ad0179f47eb7d71e719a0cded030aa0254d
jaeh <jascha@jaeh.at>
2023-07-27 12:22:22 +0200

    src/middleware/body: add warning aboud E_BODY_CHUNK_SIZE

--- src/middleware/body.mjs
+++ src/middleware/body.mjs
@@ -1,9 +1,16 @@
-// this middleware expects small chunks of data.
-// it loads the full request body into ram before returning.
-// request body size limits should be set in this middleware in the future.
+/*
+ * this middleware expects small chunks of data.
+ * it loads the full request body into ram before returning.
+ * request body size limits should be set in this middleware in the future,
+ * before it gets used anywhere!
+ */
+
+import log from '@magic/log'
 
 export const body = req =>
   new Promise((resolve, reject) => {
+    log.error('E_BODY_CHUNK_SIZE_NOT_IMPLEMENTED', 'the post request body currently has no size limitation applied to it. please add it before using this middleware!')
+
     try {
       const isJson = req.headers['content-type'] === 'application/json'
 
