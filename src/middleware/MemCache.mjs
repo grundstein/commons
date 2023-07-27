46bbcbacd46d38d00e7df969d7a902dca4fb097a
jaeh <jascha@jaeh.at>
2023-07-27 12:22:36 +0200

    add src/middleware/MemCache

---
+++ src/middleware/MemCache.mjs
@@ -0,0 +1,39 @@
+import is from '@magic/types'
+import os from 'os'
+
+const oneMegaByte = 1024 * 1024
+const hundredMegaBytes = oneMegaByte * 100
+
+export class MemCache {
+  constructor() {
+    this.data = {}
+  }
+
+  get(key) {
+    return this.data[key]
+  }
+
+  set(key, val) {
+    if (!is.buffer(val)) {
+      try {
+        val = Buffer.from(val)
+      } catch (e) {
+        log.error(e)
+        return false
+      }
+    }
+
+    const osFreeMem = os.freemem()
+    // get free memory in bytes, roughly what our val.length equals too.
+    const allFreeMem = (osFreeMem / 1024)
+
+    // make sure we either have val.length * 5 or more than 100 mb of free ram
+    // before adding a key <> value pair
+    if (allFreeMem > val.length || allFreeMem > hundredMegaBytes) {
+      this.data[key] = val
+      return true
+    }
+
+    return false
+  }
+}
--- src/middleware/index.mjs
+++ src/middleware/index.mjs
@@ -1,3 +1,4 @@
 export { body } from './body.mjs'
 export { clientError } from './clientError.mjs'
 export { listener } from './listener.mjs'
+export { MemCache } from './MemCache.mjs'
