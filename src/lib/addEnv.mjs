f034586be71f0d964de9c0d2e5a7bcaf1e9e0b64
jaeh <jascha@jaeh.at>
2023-07-29 22:14:55 +0200

    src/lib/addEnv: make sure we always return an object, use const instead of let

--- src/lib/addEnv.mjs
+++ src/lib/addEnv.mjs
@@ -2,22 +2,23 @@ export const addEnv = async file => {
   /*
    * copy process.env to make sure we do not pollute bash env vars
    */
-  let env = { ...process.env }
+  const env = { ...process.env }
 
   /*
    * add file contents to the env variables
    */
   try {
     const { env: environment } = await import(file)
-    env = {
+
+    return {
       ...environment,
       ...env,
     }
-
-    return env
   } catch (e) {
     if (e.code !== 'ERR_MODULE_NOT_FOUND') {
       console.log('error importing env', e)
     }
   }
+
+  return env
 }
