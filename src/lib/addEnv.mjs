d9e11f579b2fc68bb265a90215cf670d599f2d5b
jaeh <jascha@jaeh.at>
2023-08-05 20:39:33 +0200

    src/lib/addEnv: use .mjs for environment.mjs default path

--- src/lib/addEnv.mjs
+++ src/lib/addEnv.mjs
@@ -1,4 +1,4 @@
-export const addEnv = async (file = '/home/grundstein/environment.js') => {
+export const addEnv = async (file = '/home/grundstein/environment.mjs') => {
   /*
    * copy process.env to make sure we do not pollute bash env vars
    */
