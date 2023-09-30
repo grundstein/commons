fa27ae5bbe528860a2d8fd402676e3e8ea43cdbc
jaeh <jascha@jaeh.at>
2023-09-30 22:17:26 +0200

    src/lib/findFavicon added

---
+++ src/lib/findFavicon.js
@@ -0,0 +1,29 @@
+import fs from '@magic/fs'
+
+export const findFavicon = async () => {
+  const localFaviconPath = path.join(process.cwd(), 'src', 'favicon.ico')
+  const localFaviconExists = await fs.exists(localFaviconPath)
+
+  const localStaticFaviconPath = path.join(process.cwd(), 'src', 'static', 'favicon.ico')
+  const localStaticFaviconExists = await fs.exists(localStaticFaviconPath)
+
+  const defaultFaviconPath = path.join('.', 'src', 'favicon.ico')
+  const defaultFaviconExists = await fs.exists(defaultFaviconPath)
+
+  let faviconPath
+  if (localFaviconExists) {
+    faviconPath = localFaviconPath
+  } else if (localStaticFaviconExists) {
+    faviconPath = localStaticFaviconPath
+  } else if (defaultFaviconExists) {
+    faviconPath = defaultFaviconPath
+  }
+
+  if (faviconPath) {
+    const faviconContent = await fs.readFile(faviconPath)
+
+    return faviconContent
+  }
+
+  return false
+}
